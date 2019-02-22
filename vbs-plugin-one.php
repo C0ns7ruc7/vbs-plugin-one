9fb8c97ce89c4676e19db337a425279646284ca3
valiant <vbshekert@hotmail.com>
2019-02-22 11:53:53 +0100

    Update vbs-plugin-one.php
    + added header info

--- vbs-plugin-one.php
+++ vbs-plugin-one.php
@@ -1,9 +1,27 @@
 <!-- <vbs-plugin-one> -->
 <?php
 /**
- * Created by PhpStorm.
- * User: Vabese
- * Date: 22-2-2019
- * Time: 10:02
+ * Plugin Name: vbs-plugin-one
+ * Plugin URI:  https://git
+ * Description: Wordpress agenda plugin
+ * Version:     1
+ * Author:      Valiant Hekert
+ * Author URI:  https://author.example.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: vbspluginone
+ *
+ * {Plugin Name} is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * {Plugin Name} is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with {Plugin Name}. If not, see {License URI}.
  */
 defined( 'ABSPATH' ) or die( 'NO direct access allowed' );
