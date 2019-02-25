b041b1d072153b8f44e8d0538890806c0568e6ad
valiant <00093215@student.rocleiden.nl>
2019-02-25 15:46:02 +0100

    Update settings-callbacks.php
    + filled callbacks with code

--- admin/settings-callbacks.php
+++ admin/settings-callbacks.php
@@ -10,42 +10,103 @@
 
 defined( 'ABSPATH' ) or die( 'NO direct access allowed' );
 
-// callback: login section
-function vbsagendaplugin_callback_section_login() {
-    echo '<p>These settings enable you to customize the WP Login screen.</p>';
-}
-
-// callback: admin section
-function vbsagendaplugin_callback_section_admin() {
-    echo '<p>These settings enable you to customize the WP Admin Area.</p>';
-}
-
 // callback: text field
 function vbsagendaplugin_callback_field_text( $args ) {
-    // todo: add callback functionality..
-    echo 'This will be a text field.';
+
+    $options = get_option( 'vbsagendaplugin_options', vbsagendaplugin_options_default() );
+
+    $id    = isset( $args['id'] )    ? $args['id']    : '';
+    $label = isset( $args['label'] ) ? $args['label'] : '';
+
+    $value = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+    echo '<input id="vbsagendaplugin_options_'. $id .'" name="vbsagendaplugin_options['. $id .']" type="text" size="40" value="'. $value .'"><br />';
+    echo '<label for="vbsagendaplugin_options_'. $id .'">'. $label .'</label>';
 }
 
 // callback: radio field
 function vbsagendaplugin_callback_field_radio( $args ) {
-    // todo: add callback functionality..
-    echo 'This will be a radio field.';
+
+    $options = get_option( 'vbsagendaplugin_options', vbsagendaplugin_options_default() );
+
+    $id    = isset( $args['id'] )    ? $args['id']    : '';
+    $label = isset( $args['label'] ) ? $args['label'] : '';
+
+    $selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+    $radio_options = array(
+        'enable'  => 'Enable custom styles',
+        'disable' => 'Disable custom styles'
+    );
+
+    foreach ( $radio_options as $value => $label ) {
+        $checked = checked( $selected_option === $value, true, false );
+
+        echo '<label><input name="vbsagendaplugin_options['. $id .']" type="radio" value="'. $value .'"'. $checked .'> ';
+        echo '<span>'. $label .'</span></label><br />';
+    }
 }
 
 // callback: textarea field
 function vbsagendaplugin_callback_field_textarea( $args ) {
-    // todo: add callback functionality..
-    echo 'This will be a textarea.';
+    $options = get_option( 'vbsagendaplugin_options', vbsagendaplugin_options_default() );
+
+    $id    = isset( $args['id'] )    ? $args['id']    : '';
+    $label = isset( $args['label'] ) ? $args['label'] : '';
+
+    $allowed_tags = wp_kses_allowed_html( 'post' );
+
+    $value = isset( $options[$id] ) ? wp_kses( stripslashes_deep( $options[$id] ), $allowed_tags ) : '';
+
+    echo '<textarea id="vbsagendaplugin_options_'. $id .'" name="vbsagendaplugin_options['. $id .']" rows="5" cols="50">'. $value .'</textarea><br />';
+    echo '<label for="vbsagendaplugin_options_'. $id .'">'. $label .'</label>';
 }
 
 // callback: checkbox field
 function vbsagendaplugin_callback_field_checkbox( $args ) {
-    // todo: add callback functionality..
-    echo 'This will be a checkbox.';
+    $options = get_option( 'vbsagendaplugin_options', vbsagendaplugin_options_default() );
+
+    $id    = isset( $args['id'] )    ? $args['id']    : '';
+    $label = isset( $args['label'] ) ? $args['label'] : '';
+
+    $checked = isset( $options[$id] ) ? checked( $options[$id], 1, false ) : '';
+
+    echo '<input id="vbsagendaplugin_options_'. $id .'" name="vbsagendaplugin_options['. $id .']" type="checkbox" value="1"'. $checked .'> ';
+    echo '<label for="vbsagendaplugin_options_'. $id .'">'. $label .'</label>';
 }
 
 // callback: select field
 function vbsagendaplugin_callback_field_select( $args ) {
-    // todo: add callback functionality..
-    echo 'This will be a select menu.';
+
+    $options = get_option( 'vbsagendaplugin_options', vbsagendaplugin_options_default() );
+
+    $id    = isset( $args['id'] )    ? $args['id']    : '';
+    $label = isset( $args['label'] ) ? $args['label'] : '';
+
+    $selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+    $select_options = array(
+
+        'default'   => 'Default',
+        'light'     => 'Light',
+        'blue'      => 'Blue',
+        'coffee'    => 'Coffee',
+        'ectoplasm' => 'Ectoplasm',
+        'midnight'  => 'Midnight',
+        'ocean'     => 'Ocean',
+        'sunrise'   => 'Sunrise',
+
+    );
+
+    echo '<select id="vbsagendaplugin_options_'. $id .'" name="vbsagendaplugin_options['. $id .']">';
+
+    foreach ( $select_options as $value => $option ) {
+
+        $selected = selected( $selected_option === $value, true, false );
+
+        echo '<option value="'. $value .'"'. $selected .'>'. $option .'</option>';
+
+    }
+
+    echo '</select> <label for="vbsagendaplugin_options_'. $id .'">'. $label .'</label>';
 }
