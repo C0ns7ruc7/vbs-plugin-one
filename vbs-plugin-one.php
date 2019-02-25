890f5b68c0b6ac878105b054d337fc1e34030920
valiant <00093215@student.rocleiden.nl>
2019-02-25 12:32:53 +0100

    Update vbs-plugin-one.php
    + added settings
    + added top-level admin menu item

--- vbs-plugin-one.php
+++ vbs-plugin-one.php
@@ -3,12 +3,12 @@
  * Plugin Name: vbs-plugin-one
  * Plugin URI:  https://github.com/C0ns7ruc7/vbs-plugin-one
  * Description: Wordpress agenda plugin
- * Version:     1.3.0
+ * Version:     2.0.0
  * Author:      Valiant Hekert
  * Author URI:  https://valianthekert.wordpress.com/
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: vbspluginone
+ * Text Domain: vbsagendaplugin
  *
  * {Plugin Name} is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,4 +26,62 @@
 
 defined( 'ABSPATH' ) or die( 'NO direct access allowed' );
 
+// display the plugin settings page
+function vbsagendaplugin_display_settings_page() {
 
+    // check if user is allowed access
+    if ( ! current_user_can( 'manage_options' ) ) return;
+
+    ?>
+
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+
+            <?php
+
+            // output security fields
+            settings_fields( 'vbsagendaplugin_options' );
+
+            // output setting sections
+            do_settings_sections( 'vbsagendaplugin' );
+
+            // submit button
+            submit_button();
+
+            ?>
+
+        </form>
+    </div>
+
+    <?php
+
+}
+
+// add top-level administrative menu
+function vbsagendaplugin_add_toplevel_menu() {
+
+    /*
+        add_menu_page(
+            string   $page_title,
+            string   $menu_title,
+            string   $capability,
+            string   $menu_slug,
+            callable $function = '',
+            string   $icon_url = '',
+            int      $position = null
+        )
+    */
+
+    add_menu_page(
+        'VBSAgendaPlugin Settings',
+        'VBSAgendaPlugin',
+        'manage_options',
+        'VBSAgendaPlugin',
+        'VBSAgendaPlugin_display_settings_page',
+        'dashicons-admin-generic',
+        null
+    );
+
+}
+add_action( 'admin_menu', 'vbsagendaplugin_add_toplevel_menu' );
