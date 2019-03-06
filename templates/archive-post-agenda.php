1048a1b99e3df91c92ac38f30d53197ca59b3e86
valiant <00093215@student.rocleiden.nl>
2019-03-06 13:28:53 +0100

    Update archive-post-agenda.php
    + seporated date into seporate vallues
    + display months
    + display years

--- templates/archive-post-agenda.php
+++ templates/archive-post-agenda.php
@@ -7,10 +7,13 @@
 ?>
 
 <div class="container pt-5 pb-5">
+
     <h1><?php single_cat_title(); /** cat = category, not animals **/ ?></h1>
 
     <div class="container">
+
         <div class="row no-gutters">
+
             <?php if ( have_posts() ) : while( have_posts() ) : the_post();
 
                 $costum_meta = get_post_meta(
@@ -28,44 +31,110 @@
                     $date_skip[1] = $date_meta_array[1] - $date_meta_array_old[1]; // month
                     $date_skip[2] = $date_meta_array[2] - $date_meta_array_old[2]; // day
 
-                    // echo $date_skip[0] . '-' . $date_skip[1] . '-' . $date_skip[2];
-
                 }else{
                     $date_meta_array = explode("-", $costum_meta);
                 }
 
                 if( isset($date_skip )){
-                    while ($date_skip[2] > 1) : ?>
-                        <?php --$date_skip[2];?>
-                            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                                <div href="" class="btn btn-light card h-100">
-                                    <div class="card-body">
+
+//                    foreach ($date_skip as $dateskipped):
+
+                    while ($date_skip[0] > 0) : ?>
+
+                        <?php --$date_skip[0];?>
+
+                        <div class="col-12">
+
+                            <div class="card h-100">
+
+                                <div class="card-body">
+
                                     <?php echo (
-                                        $date_meta_array[0] . '-' .
-                                        $date_meta_array[1] . '-' .
-                                        ($date_meta_array[2] - $date_skip[2])
+                                        ($date_meta_array[0] - $date_skip[0])
                                     ); ?>
-                                    </div>
+
                                 </div>
+
                             </div>
+
+                        </div>
+
+                    <?php endwhile;
+
+                    while ($date_skip[1] > 0) : ?>
+
+                        <?php --$date_skip[1];?>
+
+                        <div class="col-12">
+
+                            <div class="card h-100">
+
+                                <div class="card-body">
+
+                                    <?php echo (
+                                        ($date_meta_array[1] - $date_skip[1])
+                                    ); ?>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    <?php endwhile; ?>
+
+                    <?php while ($date_skip[2] > 1) : ?>
+
+                        <?php --$date_skip[2];?>
+
+                            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+
+                                <div href="" class="btn btn-light card h-100">
+
+                                    <div class="card-body">
+
+                                        <?php echo (
+                                            ($date_meta_array[2] - $date_skip[2])
+                                        ); ?>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
                         <?php
+
                     endwhile;
+
+//                    endforeach;
+
                 }
+
             ?>
 
                 <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+
                     <a href="<?php the_permalink(); ?>" class="btn btn-light card h-100">
+
                         <div class="card-body">
 
-                            <h4><?php echo $costum_meta;?></h4>
-                            <h5><?php the_title(); ?></h5>
+                            <h2><?php echo $date_meta_array[2];?></h2>
+
+                            <p><?php the_title(); ?></p>
 
                         </div>
+
                     </a>
+
                 </div>
+
             <?php endwhile; endif; ?>
+
         </div>
+
     </div>
+
 </div>
 
 <?php get_footer() ?>
